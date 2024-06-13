b3b563ded595bf69e11c14f062439d2f2ad29589
Mohammad Ruman <mohammaderuman@gmail.com>
2024-06-13 10:22:20 +0530

    Test for TimesTwo

---
+++ jest testing/timeTwo.js
@@ -0,0 +1,4 @@
+function timesTwo(num){
+    return num*2;
+}
+module.exports=timesTwo;
---
+++ jest testing/timeTwo.test.js
@@ -0,0 +1,4 @@
+const timeTwo = require('./timeTwo');
+test('return number time 2',()=>{
+    expect(timeTwo(3)).toBe(6);
+});
